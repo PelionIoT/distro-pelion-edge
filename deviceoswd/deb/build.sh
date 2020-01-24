9a51239da2fe6833ab45e84fb2e1563bb463f12b
OlhaKulykGL <olha.kulyk@globallogic.com>
2020-01-24 20:39:52 +0200

    Deb:deviceoswd: Add build scripts
    Change-Id: I97f2b6aadbfc5a641c4381b446c3755fff13f9a2

---
+++ deviceoswd/deb/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Internal variables
+PELION_PACKAGE_NAME="deviceoswd"
+PELION_PACKAGE_VERSION="0.0.1" # The same value is in debian/control file
+PELION_PACKAGE_DIR=$(cd `dirname $0` && pwd)
+
+PELION_COMPONENT_NAME="deviceoswd"
+PELION_COMPONENT_URL="https://github.com/armPelionEdge/edgeos-wd.git"
+PELION_COMPONENT_VERSION="master"
+
+source $PELION_PACKAGE_DIR/../../build-env/inc/build-common.sh
+
+function main() {
+    pelion_parse_args "$@"
+
+    pelion_env_validate
+
+    pelion_source_preparation $PELION_COMPONENT_NAME $PELION_COMPONENT_URL $PELION_COMPONENT_VERSION
+    echo "INFO: Source preparation done!"
+
+    pelion_generation_deb_source_packages
+    echo "INFO: Generation debian source packages done!"
+
+    pelion_building_deb_package
+    echo "INFO: Building debian package done!"
+
+    echo "INFO: Done!"
+}
+
+# Entry point
+main $@
