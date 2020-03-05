58ec60d40669e3d9d8de2e4a2ad4f18e0d95b5db
Orest Hera <orest.hera2@globallogic.com>
2020-03-05 22:30:45 +0200

    Docker: Separate source generation and build images
    The docker image 'pelion-bionic-build' contains minimal environment for cross
    building packages.
    The image 'pelion-bionic-source' contains additional packages required for
    source generation (git, npm, python).
    
    Change-Id: I00b2e6ab48e71c5c97d8f30b5158069f025faa91

--- build-env/bin/docker-ubuntu-bionic-create.sh
+++ build-env/bin/docker-ubuntu-bionic-create.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR=$(cd "`dirname \"$0\"`" && pwd)
+CTX_PATH="$SCRIPT_DIR"/../docker/docker-ubuntu-18-bionic
 
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) \
-    -t ubuntu1804-clean "$SCRIPT_DIR"/../docker/docker-ubuntu-18-bionic
+    -t pelion-bionic-build -f "$CTX_PATH/Dockerfile.build" "$CTX_PATH"
+
+docker build \
+    -t pelion-bionic-source -f "$CTX_PATH/Dockerfile.source" "$CTX_PATH"
