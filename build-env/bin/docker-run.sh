8dbb263a1c4c6fb39675e4a4ef5fa57fcc785771
Orest Hera <orest.hera2@globallogic.com>
2020-03-05 15:52:23 +0200

    Docker: change mount of project and home directories
    Host user home directory is separated from home directory of docker user.
    
    Mount points:
    * Root of project repo: /pelion-build
    * Host home directory: /mnt/home
    * Host SSH keys (~/.ssh): /home/user/.ssh
    
    Helper bash aliases:
    * cdr: go to pelion root directory '/pelion-build'
    * cdh: go to mounted host home directory '/mnt/home'
    
    Change-Id: I2340d01760d80e589a32cf70f07bf87d43aed20e

--- build-env/bin/docker-run.sh
+++ build-env/bin/docker-run.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-docker run -it -v "$HOME":/home/user $@
+ROOT_DIR=$(cd "`dirname \"$0\"`"/../.. && pwd)
+
+docker run -it \
+    -v "$HOME/.ssh":/home/user/.ssh \
+    -v "$HOME":/mnt/home \
+    -v "$ROOT_DIR":/pelion-build \
+    "$@"
+
