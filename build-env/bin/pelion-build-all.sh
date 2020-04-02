2b93f26b4b801dafbdae72a9c7afaefae8f9577c
Vasily Smirnov <vasilii.smirnov@globallogic.com>
2020-04-03 00:49:52 +0300

    Build system: Add the --tar flag to pelion-build-all.sh
    Change-Id: I4c0acb62d5f7c90e747135b27825753aced4c681

--- build-env/bin/pelion-build-all.sh
+++ build-env/bin/pelion-build-all.sh
@@ -33,6 +33,7 @@ METAPACKAGES=(
 
 PELION_PACKAGE_SOURCE=false
 PELION_PACKAGE_BUILD=false
+PELION_TARBALL=false
 PELION_PACKAGE_DOCKER=false
 PELION_BUILD_OPT=''
 PELION_ARCHS=( 'amd64' )
@@ -56,6 +57,10 @@ function pelion_parse_args() {
                 PELION_PACKAGE_SOURCE=true
                 ;;
 
+            --tar)
+                PELION_TARBALL=true
+                ;;
+
             --docker)
                 PELION_PACKAGE_DOCKER=true
                 ;;
@@ -66,22 +71,24 @@ function pelion_parse_args() {
                 echo "Options:"
                 echo " --source            Generate source package."
                 echo " --build             Build binary from source generated with --source option."
+                echo " --tar               Build a tarball from Debian packages."
                 echo " --docker            Use docker containers."
                 echo " --install           Install build dependencies."
                 echo " --arch=<arch>       Set comma-separated list of target architectures."
                 echo " --help,-h           Print this message."
                 echo ""
-                echo "If neither '--source' nor '--build' option is specified both are activated."
+                echo "If none of '--source', '--build' or '--tar' options are specified,"
+                echo "all of them are activated."
                 echo ""
 
                 exit 0
                 ;;
         esac
     done
-
-    if ! $PELION_PACKAGE_SOURCE && ! $PELION_PACKAGE_BUILD; then
+    if ! $PELION_PACKAGE_SOURCE && ! $PELION_PACKAGE_BUILD && ! $PELION_TARBALL; then
         PELION_PACKAGE_SOURCE=true
         PELION_PACKAGE_BUILD=true
+        PELION_TARBALL=true
     fi
 
     if $PELION_PACKAGE_DOCKER; then
@@ -91,25 +98,45 @@ function pelion_parse_args() {
     fi
 }
 
-pelion_parse_args $@
+pelion_parse_args "$@"
 
 if $PELION_PACKAGE_SOURCE; then
-    for p in ${PACKAGES[@]}; do
+    for p in "${PACKAGES[@]}"; do
         echo "Generating source package of '$p'"
-        "$SCRIPT_DIR"/../../$p/deb/build.sh $PELION_BUILD_OPT --source
+        "$SCRIPT_DIR"/../../"$p"/deb/build.sh $PELION_BUILD_OPT --source
     done
 fi
 
 if $PELION_PACKAGE_BUILD; then
     for arch in "${PELION_ARCHS[@]}"; do
-        for p in ${PACKAGES[@]}; do
+        for p in "${PACKAGES[@]}"; do
             echo "Building '$p' for '$arch'"
-            "$SCRIPT_DIR"/../../$p/deb/build.sh $PELION_BUILD_OPT --arch=$arch --build
+            "$SCRIPT_DIR"/../../"$p"/deb/build.sh $PELION_BUILD_OPT --arch="$arch" --build
         done
+    done
+
+    for p in "${METAPACKAGES[@]}"; do
+        echo "Generating '$p'"
+        "$SCRIPT_DIR"/../../metapackages/"$p"/deb/build.sh $PELION_BUILD_OPT
     done
 fi
 
-for p in ${METAPACKAGES[@]}; do
-    echo "Generating '$p'"
-    "$SCRIPT_DIR"/../../metapackages/$p/deb/build.sh $PELION_BUILD_OPT
-done
+if $PELION_TARBALL; then
+    cd "$SCRIPT_DIR"/../..
+    CONTAINER=pelion-bionic-source
+
+    if $PELION_PACKAGE_DOCKER && [ -z "$(docker images -q $CONTAINER)" ]; then
+        ./build-env/bin/docker-ubuntu-bionic-create.sh
+    fi
+
+    for arch in "${PELION_ARCHS[@]}"; do
+        if $PELION_PACKAGE_DOCKER; then
+            docker run --rm \
+                -v "$HOME/.ssh":/home/user/.ssh \
+                -v "$(pwd)":/pelion-build \
+                $CONTAINER ./build-env/bin/deb2tar.sh --arch="$arch"
+        else
+            ./build-env/bin/deb2tar.sh --arch="$arch"
+        fi
+    done
+fi
