5d13ce238179a68391c5d5953f56f92ab9558953
Orest Hera <orest.hera2@globallogic.com>
2020-04-23 12:39:18 +0300

    Docker: Create source and build images for Debian 9
    Change-Id: I269b75de45984fd4cc92234ddbc9fcbe3d4a8a17

--- build-env/bin/docker-debian-stretch-create.sh
+++ build-env/bin/docker-debian-stretch-create.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR=$(cd "`dirname \"$0\"`" && pwd)
+CTX_PATH="$SCRIPT_DIR"/../docker/docker-debian-9-stretch
 
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) \
-    -t debian9-clean "$SCRIPT_DIR"/../docker/docker-debian-9-stretch
+    -t pelion-stretch-build -f "$CTX_PATH/Dockerfile.build" "$CTX_PATH"
+
+docker build \
+    -t pelion-stretch-source -f "$CTX_PATH/Dockerfile.source" "$CTX_PATH"
