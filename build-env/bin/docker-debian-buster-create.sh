1a376f00c47582f8bb9df800ee0ccedba28a3679
Orest Hera <orest.hera2@globallogic.com>
2020-05-20 03:23:44 +0300

    Docker: Create Debian 10 build and source images
    Docker images contain local apt repo with the package nodejs of version
    8.11.4-1nodesource1 that is used instead of standard system nodejs
    packages.
    
    Change-Id: Ic2a87fa8eb9c0474b734ee71646ef71729bd021c

---
+++ build-env/bin/docker-debian-buster-create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_DIR=$(cd "`dirname \"$0\"`" && pwd)
+CTX_PATH="$SCRIPT_DIR"/../docker
+DOCKER_FILE_PATH="$CTX_PATH"/docker-debian-10-buster
+
+docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) \
+    -t pelion-buster-build -f "$DOCKER_FILE_PATH/Dockerfile.build" "$CTX_PATH"
+
+docker build \
+    -t pelion-buster-source -f "$DOCKER_FILE_PATH/Dockerfile.source" "$CTX_PATH"
