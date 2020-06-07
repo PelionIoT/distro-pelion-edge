47bca0af86e3d9ce1adabfeb716eb9725e23e871
Mo Chen <mo.chen@arm.com>
2020-06-07 17:29:00 -0500

    Fixes for edge-core devmode
    -Patch update script locations
    -Add placeholder update scripts
    -Build using developer mode credential files

--- mbed-edge-core-devmode/deb/build.sh
+++ mbed-edge-core-devmode/deb/build.sh
@@ -14,6 +14,8 @@ source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 function pelion_mbed_edge_core_source_preparation_cb() {
     cd "$PELION_SOURCE_DIR/mbed-edge"
     git submodule update --init --recursive
+    cp "$PELION_PACKAGE_DIR/../../mbed_cloud_dev_credentials.c" "$PELION_SOURCE_DIR/mbed-edge/config/mbed_cloud_dev_credentials.c"
+    cp "$PELION_PACKAGE_DIR/../../update_default_resources.c" "$PELION_SOURCE_DIR/mbed-edge/config/update_default_resources.c"
 }
 
 pelion_main "$@"
