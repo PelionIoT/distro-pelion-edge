578fdf22d3004ce028b4691c6dd2f0a0a9bdea22
J. Michael Welsh <mike.welsh@arm.com>
2020-08-27 15:20:52 -0500

    Latest Maestro
    Updating the maestro build hash to the lastest dev work.

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="1968345aa18770bedc20a971b21b767a6c5dceba")
+    ["git@github.com:armPelionEdge/maestro.git"]="300fcdf67616f4018f0f2b53db4b88c54a333e1b")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
