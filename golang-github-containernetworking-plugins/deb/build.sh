10e41d14e6a190896edc96490dc3b66fffa4efa0
Ryan Nowakowski <ryan.nowakowski@arm.com>
2020-09-03 12:28:33 -0500

    added golang-github-containernetworking-plugins debian/v0.8.4
    Added package golang-github-containernetworking-plugins at version debian/v0.8.4.
    
    https://salsa.debian.org/go-team/packages/golang-github-containernetworking-plugins/-/tree/debian/0.8.4-1

---
+++ golang-github-containernetworking-plugins/deb/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Internal variables
+PELION_PACKAGE_NAME="golang-github-containernetworking-plugins"
+PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
+
+declare -A PELION_PACKAGE_COMPONENTS=(
+    ["git@github.com:containernetworking/plugins.git"]="v0.8.4")
+
+source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
+
+pelion_main "$@"
