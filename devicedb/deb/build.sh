757fac95f14a6d9a99ec59c8b2d6e732580e2d61
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-09-03 12:21:23 -0500

    DeviceDB service and build files
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- devicedb/deb/build.sh
+++ devicedb/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="devicedb"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/devicedb.git"]="a4ee46f3d9bc58b694e7563502142dfe265110cb")
+    ["git@github.com:armPelionEdge/devicedb.git"]="c87990f46956c0703d809257fa3fc95ee58c41cf")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
