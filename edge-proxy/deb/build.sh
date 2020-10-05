6b2fd55630443cbd2c9d93095b022c4927db2aef
Nic Costa <nic.costa@gmail.com>
2020-10-05 13:25:30 +0000

    upgrade edge-proxy
    this allows us to remove the edge-proxy patch file

--- edge-proxy/deb/build.sh
+++ edge-proxy/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="edge-proxy"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/edge-proxy.git"]="883e6cb16db4a5b97302c54ba36ee5566ea8557b")
+    ["git@github.com:armPelionEdge/edge-proxy.git"]="b0f66f21e84078ff52e11f59f1cc9890a0dfaa34")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
