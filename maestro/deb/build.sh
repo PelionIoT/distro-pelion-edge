e623167fde6b8b1d6b49107cc41a95173207b596
Nic Costa <nic.costa@gmail.com>
2020-10-20 16:46:13 +0000

    update maestro
    pull latest version of maestro for:
    - devicedb integration fixes
    - fix for nameserver entries in maestro being confused with interface entries
    - other bug fixes
    
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="d95b7cec54c52b4b7a241b9c0151b744004e385b")
+    ["git@github.com:armPelionEdge/maestro.git"]="59d781b6b7cc4330889c45ac1aee8d713af6f22b")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
