20b9854c51bccd0f4e7f5580b2efa17abb127c7a
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-10-20 16:11:40 -0500

    Update maestro to the greatest code base yet
    This will include the fix for nameserver entries in maestro being
    confused with interface entries.
    
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="bc9595b3164830b325884196e6fc5571d90489aa")
+    ["git@github.com:armPelionEdge/maestro.git"]="80fb3c7e1644501e685d20be304cdb1c7600c414")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
