501b6565f209697045435d22774993e3dae24f3d
Nic Costa <nic.costa@gmail.com>
2020-10-20 16:46:13 +0000

    update maestro
    pull latest version of maestro for devicedb integration fixes

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="d95b7cec54c52b4b7a241b9c0151b744004e385b")
+    ["git@github.com:armPelionEdge/maestro.git"]="bc9595b3164830b325884196e6fc5571d90489aa")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
