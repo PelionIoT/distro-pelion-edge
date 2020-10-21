92412baaae36b2a6c3b85c6fac13ba8751c45b5a
Nic Costa <nic.costa@gmail.com>
2020-10-21 17:56:48 +0000

    update maestro
    pull in another bug fix

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="80fb3c7e1644501e685d20be304cdb1c7600c414")
+    ["git@github.com:armPelionEdge/maestro.git"]="59d781b6b7cc4330889c45ac1aee8d713af6f22b")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
