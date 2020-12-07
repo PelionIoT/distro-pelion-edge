664649f9366d1256935084d0d4e654991c0af18b
Ryan Nowakowski <ryan.nowakowski@arm.com>
2020-12-07 16:58:51 -0600

    Use ssh-agent if available instead of mounting $HOME/.ssh
    Some users like to keep their SSH identity in an ssh agent instead of
    passphraseless on disk.  If an SSH agent is available, use that for ssh
    authentication instead of volume mounting $HOME/.ssh.

--- build-env/bin/docker-run.sh
+++ build-env/bin/docker-run.sh
@@ -7,8 +7,14 @@ APT_REPO="$ROOT_DIR"/build/apt/$DOCKER_DIST
 echo "Using APT repo dir: " $APT_REPO
 mkdir -p $APT_REPO
 
+if [ -n "$SSH_AUTH_SOCK" ]; then
+	SSH_ARGS="-v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
+else
+	SSH_ARGS="-v \"$HOME/.ssh\":/home/user/.ssh"
+fi
+
 docker run -it \
-    -v "$HOME/.ssh":/home/user/.ssh \
+    $SSH_ARGS \
     -v "$HOME":/mnt/home \
     -v "$ROOT_DIR":/pelion-build \
     -v "$APT_REPO":/opt/apt-repo \
