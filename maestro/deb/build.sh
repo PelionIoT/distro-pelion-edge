d5949d2794942fc4d391e2b83bbdf09c32691f10
Michael Ray <michael.ray@arm.com>
2020-12-11 12:39:06 -0600

    Bump maestro hash
    Fixes a bug where maestro needs a capital letter in the item added
    to /etc/resolv.conf when using resolvconf

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/maestro.git"]="59d781b6b7cc4330889c45ac1aee8d713af6f22b")
+    ["git@github.com:armPelionEdge/maestro.git"]="a0adef8c579db2eaa15f08d1813eef417850f7d6")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
