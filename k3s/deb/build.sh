53d849ad0a77a96258fe7b052a72d5c204e11765
J. Michael Welsh <mike.welsh@arm.com>
2021-02-11 20:46:44 +0000

    Initial working build
    This is the initial working build of k3s.

--- k3s/deb/build.sh
+++ k3s/deb/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Internal variables
-PELION_PACKAGE_NAME="kubelet"
+PELION_PACKAGE_NAME="k3s"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/edge-kubelet.git"]="83b266ae6939012883611d6dbda745f2490a67c4")
+    ["https://github.com/rancher/k3s.git"]="21d1690d5da50135cf3a4b9ccd278b1d037d52cc")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
