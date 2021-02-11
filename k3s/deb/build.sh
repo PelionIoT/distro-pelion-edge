eea08267cb9c3b1330759191b43c9d77991f35a4
Michael Ray <michael.ray@arm.com>
2021-02-11 11:47:24 -0600

    Added k3s debian package - DOESNT WORK YET

---
+++ k3s/deb/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Internal variables
+PELION_PACKAGE_NAME="kubelet"
+PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
+
+declare -A PELION_PACKAGE_COMPONENTS=(
+    ["git@github.com:armPelionEdge/edge-kubelet.git"]="83b266ae6939012883611d6dbda745f2490a67c4")
+
+source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
+
+string="$@"
+
+if [[ $string == *"--arch=arm64"* ]]; then
+    export OS_CPU="linux/arm64"
+else
+    if [[ $string == *"--arch=armhf"* ]]; then
+        export OS_CPU="linux/arm"
+    else
+        export OS_CPU="linux/amd64"
+    fi
+fi
+
+echo "buildtype=$OS_CPU"
+
+pelion_main "$@"
---
+++ k3s/deb/debian/launch-kubelet.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+exec /usr/bin/k3s \
+    --config=/etc/pelion/k3s_config.yaml
