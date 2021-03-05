4bb3eb7994c955b71743eb5e133be386f62c282b
Ryan Nowakowski <ryan.nowakowski@arm.com>
2021-03-05 14:51:31 -0600

    transition github.com urls from ssh to https
    Since all of these repos are public now, let's not require ssh to avoid
    the hassle of ssh public key-based auth

--- devicedb/deb/build.sh
+++ devicedb/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="devicedb"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:armPelionEdge/devicedb.git"]="d24df289ab24a035ebf64d2ed27a2d531a2319da")
+    ["https://github.com/armPelionEdge/devicedb.git"]="d24df289ab24a035ebf64d2ed27a2d531a2319da")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
