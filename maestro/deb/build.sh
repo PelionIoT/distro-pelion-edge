9b8658521f5f00b69d39774208c39d4014705c44
Michael Ray <mray190@gmail.com>
2021-03-10 11:27:08 -0600

    Bump maestro hash to maestro v2.10.0

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/maestro.git"]="a0adef8c579db2eaa15f08d1813eef417850f7d6")
+    ["https://github.com/armPelionEdge/maestro.git"]="20caa5d032424a11146b7923eaaed74e80de96da")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
