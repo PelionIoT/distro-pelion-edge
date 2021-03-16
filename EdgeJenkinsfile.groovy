92369685dc0f7da6fc28777a1a75e53f03438b8c
Kunal Sharma <kunal.sharma@arm.com>
2021-03-16 19:01:34 +0530

    this file will be required on branch where PR checks have to be triggered

---
+++ EdgeJenkinsfile.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+@Library('edge-ci') _
+
+//linuxPelionEdge Function is implemented in edge-ci library which is currently a private repo and used for internal testing only
+linuxPelionEdge()
