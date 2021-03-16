878eb46c439dfb608e99162a30ab6b4e22d0b16c
Ryan Nowakowski <ryan.nowakowski@arm.com>
2021-03-16 18:16:01 -0500

    Ensure apt repo directory permissions are correct
    When creating directories as the host user, we can't assume that the
    user hasn't changed their umask to be more restrictive.  So we should
    explicitly set the umask to ensure it's consistent.  Example: Without
    this the build can fail if the user's umask is 026

--- build-env/bin/docker-run.sh
+++ build-env/bin/docker-run.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(cd "`dirname \"$0\"`"/../.. && pwd)
 DOCKER_DIST=$(echo $1 | cut -d- -f2)
 APT_REPO="$ROOT_DIR"/build/apt/$DOCKER_DIST
 echo "Using APT repo dir: " $APT_REPO
-mkdir -p $APT_REPO
+mkdir -p -m 755 $APT_REPO
 
 if [ -n "$SSH_AUTH_SOCK" ]; then
 	# Docker for Mac requires a magic string instead of the value of SSH_AUTH_SOCK - see https://github.com/docker/for-mac/issues/410#issuecomment-537127831
