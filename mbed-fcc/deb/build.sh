dd180de110367d899f9893582b5e18ee85de1cf6
Nic Costa <nic.costa@gmail.com>
2021-03-23 16:34:46 -0500

    mbed-fcc: add pal configuration file similar to mbed-edge
    This fixes a critical issue encountered by mbed-fcc when provisioning
    from a CBOR file within a debian-based docker container on a CentOS7 host.
    
    [ERR ][PAL ]: pal_RTOSInitialize: pal_plat_RTOSInitialize failed, status=fffff084
    
    All settings in the config file are the same as the default values
    provided by factory-configurator-client-example/source/pal_config_linux.h
    except for PAL_SIMULATOR_TEST_ENABLE 1 which causes
    pal_plat_osThreadCreate() to create threads with attributes inherited
    from the calling thread rather than explicitly configured attributes,
    specifically the SCHED_RR scheduling policy, which cause fatal issues
    on some platforms.

--- mbed-fcc/deb/build.sh
+++ mbed-fcc/deb/build.sh
@@ -5,6 +5,7 @@ PELION_PACKAGE_NAME="mbed-fcc"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 PELION_PACKAGE_SKIP_DEPS_CHECKING=true
+PELION_PACKAGE_SOURCE_PREPARATION_CALLBACK=pelion_mbed_fcc_source_preparation_cb
 PELION_PACKAGE_ORIGIN_SOURCE_UPDATE_CALLBACK=pelion_mbed_fcc_origin_source_update_cb
 
 declare -A PELION_PACKAGE_COMPONENTS=(
@@ -12,6 +13,11 @@ declare -A PELION_PACKAGE_COMPONENTS=(
     ["https://github.com/ARMmbed/mbed-cloud-client.git"]="4.6.0")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
+
+function pelion_mbed_fcc_source_preparation_cb() {
+    cd "$PELION_SOURCE_DIR/$PELION_PACKAGE_NAME/factory-configurator-client-example"
+    cp "$PELION_PACKAGE_DIR/debian/files/sotp_fs_linux.h" "./source/pal_config_linux.h"
+}
 
 function pelion_mbed_fcc_origin_source_update_cb() {
     mv "$PELION_TMP_BUILD_DIR/$PELION_PACKAGE_FOLDER_NAME/factory-configurator-client-example"/* "$PELION_TMP_BUILD_DIR/$PELION_PACKAGE_FOLDER_NAME"
