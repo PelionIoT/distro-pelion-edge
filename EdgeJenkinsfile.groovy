74d13db5a1b42900a742f9c5828b33ed34079507
kunal11191 <kunal.sharma@arm.com>
2021-03-30 10:31:48 +0530

    testing new build process on master

---
+++ EdgeJenkinsfile.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+@Library('edge-ci@new_build_process') _
+
+//linuxPelionEdge Function is implemented in edge-ci library which is currently a private repo and used for internal testing only
+linuxPelionEdge()
