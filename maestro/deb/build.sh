b6aca387b88d0e2971f0a38249923b2510e28f43
Krzysztof Bembnista <krzysztof.bembnista@globallogic.com>
2021-05-31 12:32:34 +0200

    Fix maestro build issue on Ubuntu Focal.
    Python package does not provide dependency with 'python' command
    anymore. As maestro dependency relies on 'python' command, it has
    to be created before running the build. Moreover python2 is required (if
    fails to build when python3 is in use).

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -4,6 +4,8 @@
 PELION_PACKAGE_NAME="maestro"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
+PELION_PACKAGE_PRE_BUILD_CALLBACK='select_python 2'
+
 declare -A PELION_PACKAGE_COMPONENTS=(
     ["https://github.com/armPelionEdge/maestro.git"]="20caa5d032424a11146b7923eaaed74e80de96da")
 
