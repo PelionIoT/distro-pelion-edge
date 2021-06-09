fbc0da2223651293cfd8702839e0b17941d14c00
Yash Goyal <yash.goyal@arm.com>
2021-06-09 13:38:16 -0500

    Upgraded pe-utils to 2.0.6

--- pe-utils/deb/build.sh
+++ pe-utils/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="pe-utils"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/pe-utils.git"]="6a436d6986c67f36936a40d31f43462c97b6f615")
+    ["https://github.com/armPelionEdge/pe-utils.git"]="cccc67643c2cf0846731573ca0c69faf68a5eb6d")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
