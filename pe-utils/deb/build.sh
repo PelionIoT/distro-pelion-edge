d89afd43fe1e9583c35324f9ab9fade8ec5a9419
Yash Goyal <yash.goyal@arm.com>
2021-06-10 12:30:41 -0500

    Updated pe-utils to 2.0.7

--- pe-utils/deb/build.sh
+++ pe-utils/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="pe-utils"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/pe-utils.git"]="cccc67643c2cf0846731573ca0c69faf68a5eb6d")
+    ["https://github.com/armPelionEdge/pe-utils.git"]="f7d3e32373eb5ed8536f70110406ad35c2e5e0cb")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
