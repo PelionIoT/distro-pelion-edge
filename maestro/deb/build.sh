d8a08908587de38ee0b89cc626d817474d230eef
Mo Chen <mo.chen@arm.com>
2021-07-12 17:15:10 -0500

    Update maestro to 3.0.0
    -Remove libgrease dependency.
    -Use go modules.

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -7,7 +7,7 @@ PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 PELION_PACKAGE_PRE_BUILD_CALLBACK='select_python 2'
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/maestro.git"]="20caa5d032424a11146b7923eaaed74e80de96da")
+    ["https://github.com/armPelionEdge/maestro.git"]="c4f079012a8c2700e8952f759dd00b931c26c574")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
