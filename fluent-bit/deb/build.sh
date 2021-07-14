98958920c1c0f8366e876b1f06cbe044603d308d
Michael Ray <michael.ray@arm.com>
2021-07-14 09:33:23 -0500

    Added fluent-bit v1.7.9
    New daemon running in the snap: fluent-bit
    Configuration staged in etc/pelion/fluent-bit.conf
    Utilizes fluent-bit v1.7.9 from github

---
+++ fluent-bit/deb/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Internal variables
+PELION_PACKAGE_NAME="fluent-bit"
+PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
+
+PELION_PACKAGE_SOURCE_PREPARATION_CALLBACK=pelion_fluent_bit_source_preparation_cb
+
+declare -A PELION_PACKAGE_COMPONENTS=(
+    ["https://github.com/fluent/fluent-bit.git"]="v1.7.9")
+source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
+
+function pelion_fluent_bit_source_preparation_cb() {
+    cd "$PELION_SOURCE_DIR/$PELION_PACKAGE_NAME/$PELION_PACKAGE_NAME"
+    # fluent-bit source has a debian folder already checked into the repository
+    # Remove the fluent-bit debian folder so we can use ours
+    # (distro-pelion-edge debian folder gets copied over after this callback)
+    rm -rf debian
+}
+
+pelion_main "$@"
