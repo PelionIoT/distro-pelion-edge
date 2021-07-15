83eabd7fd446689b59ede31ccb39024a58d337b2
Mo Chen <mo.chen@arm.com>
2021-07-15 12:27:51 -0500

    Update to latest dev sha for maestro.

--- maestro/deb/build.sh
+++ maestro/deb/build.sh
@@ -7,7 +7,7 @@ PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 PELION_PACKAGE_PRE_BUILD_CALLBACK='select_python 2'
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/maestro.git"]="c4f079012a8c2700e8952f759dd00b931c26c574")
+    ["https://github.com/armPelionEdge/maestro.git"]="d6f1e29c2994c34adcf544d0fff50fd31fd01cdb")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
