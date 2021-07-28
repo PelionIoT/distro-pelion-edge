10690cf4b82803d41a1ed7ae4f6c526e28ca768b
J. Michael Welsh <mike.welsh@pelion.com>
2021-07-28 19:01:08 +0000

    Update mbed-edge-core-devmode to 0.18
        Updated mbed-edge-core-devmode to version 0.18 and updated all of the patches to
        work with the changes in the new versions code.

--- mbed-edge-core-devmode/deb/build.sh
+++ mbed-edge-core-devmode/deb/build.sh
@@ -7,7 +7,7 @@ PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 PELION_PACKAGE_SOURCE_PREPARATION_CALLBACK=pelion_mbed_edge_core_source_preparation_cb
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/ARMmbed/mbed-edge.git"]="0.13.0")
+    ["https://github.com/ARMmbed/mbed-edge.git"]="0.18.0")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
@@ -19,6 +19,10 @@ function pelion_mbed_edge_core_source_preparation_cb() {
     cp "$PELION_PACKAGE_DIR/debian/files/sotp_fs_linux.h" "./config/sotp_fs_linux.h"
     cp "$PELION_PACKAGE_DIR/debian/files/osreboot.c" "./edge-core/osreboot.c"
     cp "$PELION_PACKAGE_DIR/debian/files/mbed_cloud_client_user_config.h" "./config/mbed_cloud_client_user_config.h"
+
+    # update the mbed-cloud-client library
+    sed -i 's!/dev/random!/dev/urandom!' lib/mbed-cloud-client/mbed-client-pal/Source/Port/Reference-Impl/OS_Specific/Linux/Board_Specific/TARGET_x86_x64/pal_plat_x86_x64.c || true
+    sed -i 's!\(MAX_RECONNECT_TIMEOUT\).*!\1 60!' lib/mbed-cloud-client/mbed-client/mbed-client/m2mconstants.h || true
 }
 
 pelion_main "$@"
