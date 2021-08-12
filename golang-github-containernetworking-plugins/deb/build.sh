fbdde31057a1694d802200eb7edb3db960b8b4ec
Michael Ray <michael.ray@arm.com>
2021-08-12 14:44:33 -0500

    Change containernetworking to c2d variant
    Kubelet was reporting the following errors without using the c2d variant:
    2145 cni.go:188] Unable to update cni config: No networks found in /etc/cni/net.d
    2145 kubelet.go:2181] Container runtime network not ready: NetworkReady=false reason:NetworkPluginNotReady message:docker: network plugin is not ready: cni config uninitialized
    
    This was due to a name clash where we were creating a containernetworking plugin
    but the containernetworking plugin already existed.
    
    The solution was to rename our package to containernetworking-c2d due to the
    fact that our c2d plugin had c2d networking files. Then, install both our
    containernetworking-c2d as well as the existing containernetworking
    
    Note: Changed kubelet and k3s to use the new plugin

--- golang-github-containernetworking-plugins/deb/build.sh
+++ golang-github-containernetworking-plugins/deb/build.sh
@@ -4,7 +4,7 @@
 PELION_PACKAGE_NAME="golang-github-containernetworking-plugins"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
-PELION_PACKAGE_BINARY_NAME="containernetworking-plugins"
+PELION_PACKAGE_BINARY_NAME="containernetworking-plugins-c2d"
 
 declare -A PELION_PACKAGE_COMPONENTS=(
     ["https://github.com/containernetworking/plugins.git"]="v0.8.4")
