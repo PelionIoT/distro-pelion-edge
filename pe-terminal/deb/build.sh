73c5a43f78c99d767c910a7c67a47481b7e90b36
adwardstark <aditya.awasthi@arm.com>
2021-09-13 14:37:33 +0530

    Added source-tag for terminal, updated version to match the release

--- pe-terminal/deb/build.sh
+++ pe-terminal/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="pe-terminal"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["git@github.com:PelionIoT/pe-terminal.git"]="8d2a020eb0bccf40cd9d9e54188e29df531bc019")
+    ["https://github.com/PelionIoT/pe-terminal.git"]="v1.0.0")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
