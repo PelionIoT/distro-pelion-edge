9f0b67189befb778b20969bf2632d62a962b8768
Krzysztof Bembnista <krzysztof.bembnista@globallogic.com>
2021-09-29 11:26:31 +0200

    update kubelet version

--- kubelet/deb/build.sh
+++ kubelet/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="kubelet"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/edge-kubelet.git"]="83b266ae6939012883611d6dbda745f2490a67c4")
+    ["https://github.com/armPelionEdge/edge-kubelet.git"]="f8ebb1afd620f6a29691a21656a1bc3b54283906")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
