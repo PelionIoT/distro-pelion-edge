d1d816c4f642cce7a9073ab0a21e6e187efb13d5
Nic Costa <nic.costa@gmail.com>
2022-03-07 15:11:07 +0000

    fix edge-proxy github org
    edge-proxy was moved from ArmPelionEdge to PelionIoT

--- edge-proxy/deb/build.sh
+++ edge-proxy/deb/build.sh
@@ -5,7 +5,7 @@ PELION_PACKAGE_NAME="edge-proxy"
 PELION_PACKAGE_DIR=$(cd "`dirname \"$0\"`" && pwd)
 
 declare -A PELION_PACKAGE_COMPONENTS=(
-    ["https://github.com/armPelionEdge/edge-proxy.git"]="8073dda0f9fc0c0906493089792faa48dc27a569")
+    ["https://github.com/PelionIoT/edge-proxy.git"]="8073dda0f9fc0c0906493089792faa48dc27a569")
 
 source "$PELION_PACKAGE_DIR"/../../build-env/inc/build-common.sh
 
